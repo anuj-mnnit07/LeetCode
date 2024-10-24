ac0af3e2b70b5d4a74174f3a7358ee44d16886ac
Anuj Kumar <anuj.mnnit07@gmail.com>
2024-10-25 00:02:10 +0400

    fix script and add readme
    Signed-off-by: Anuj Kumar <anuj.mnnit07@gmail.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+Create all source file, header and test files for source code using below command:
+
+```bash
+./create.sh -f <file_name> -h hpp
+```
+
+For running use below command:
+
+```bash
+mkdir build && cd build
+cmake ..
+```
+Open LeetCode.sln folder in Visual Studio and Run the project from there.
--- create.sh
+++ create.sh
@@ -1,7 +1,39 @@
 #! /bin/bash
 
-file_name=27_remove_element
+
+file_name=""
 header_extension=hpp
+
+while getopts ":f:h:" opt; do
+  case $opt in
+    f) file_name="$OPTARG"
+    ;;
+    h) header_extension="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) echo "Option $opt needs valid arg -f for filename and -h for header externsion hpp or h"
+    exit 1
+    ;;
+  esac
+done
+
+if [ "${file_name}" == "" ];then
+  echo "Error: file_name is empty, add argument using -f file_name"
+  exit 1
+fi
+
+if [ "${header_extension}" != "hpp" ] && [ "${header_extension}" != "h" ]; then
+  echo "Error: only header extension possible is h or hpp"
+  exit 1
+fi
+
+echo "file_name: $file_name"
+echo "header_extension: $header_extension"
 
 FILE_NAME=${file_name^^}
 HEADER_EXTENSION=${header_extension^^}
@@ -9,7 +41,7 @@ HEADER_EXTENSION=${header_extension^^}
 echo $FILE_NAME
 
 cat <<EOF >> source/include/${file_name}.${header_extension}
-#ifdef __${FILE_NAME}_${HEADER_EXTENSION}__
+#ifndef __${FILE_NAME}_${HEADER_EXTENSION}__
 #define __${FILE_NAME}_${HEADER_EXTENSION}__
 
 #endif
